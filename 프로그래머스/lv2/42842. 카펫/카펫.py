f359d3e9d9182f8acdc33764acf3d7bcad72d771
leejiseop <lkj0245@naver.com>
2023-09-02 22:55:49 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,6 +1,6 @@
 def solution(brown, yellow):
     for i in range(1, int(yellow**0.5) + 1):
-        y_h = i
         y_w = yellow / i
+        y_h = i
         if (y_w * 2) + (y_h * 2) + 4 == brown:
             return [y_w + 2, y_h + 2]
