311cf601e60ba94d6528ca044d356d4949702ac2
leejiseop <lkj0245@naver.com>
2023-09-02 21:21:46 +0900

    [level 2] Title: 소수 찾기, Time: 0.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,7 +1,7 @@
 import itertools
 
 def is_decimal(number):
-    if number == 1:
+    if number <= 1:
         return False
     
     for i in range(2, int(number**1/2) + 1):
@@ -13,15 +13,14 @@ def is_decimal(number):
 def solution(numbers):
     answer = 0
     p = []
+    
     for i in range(1, len(numbers) + 1):
-        p += list(set(itertools.permutations(list(numbers), i)))
-    final_p = [int(''.join(i)) for i in p if i[0] != '0']
+        p += list(itertools.permutations(list(numbers), i))
+        
+    final_p = set([int(''.join(i)) for i in p])
+    
     for num in final_p:
         if is_decimal(num):
             answer += 1
-    
-    # 모든 조합 만들고 set 처리
-    # 0으로 시작하는것 빼기
-    # 하나씩 전부 소수판별 후 개수 리턴
     
     return answer
