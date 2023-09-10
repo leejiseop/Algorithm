f8757a4bb0efbf4443a164e440d664f3ded5d1dc
leejiseop <lkj0245@naver.com>
2023-09-11 03:08:53 +0900

    [level 3] Title: 입국심사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,19 +1,25 @@
 def solution(n, times):
     left = 1
-    right = max(times) * n
+    right = min(times) * n
     
     while left <= right:
-        mid = (left+right)//2
+        mid = (left + right) // 2
         people = 0
      	
         for time in times:
-            people += mid//time
-            
-            if people >= n:
+            people += mid // time
+            if n <= people:
                 break
-        if people >= n: # times = [10 12 15], mid = 20
+                
+        if n <= people: # case: 'times = [10 12 15], mid = 20'
             answer = mid
-            right = mid -1
+            right = mid - 1
         else :
             left = mid + 1
     return answer
+    #     if total >= n:
+    #         end = mid
+    #     else:
+    #         start = mid + 1
+    # answer = start
+    # return answer
