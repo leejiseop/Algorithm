cb8dc7556bfdbfcba99616c302f48c67e5b1e94e
leejiseop <lkj0245@naver.com>
2023-09-16 01:23:10 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -2,23 +2,58 @@ def solution(N, number):
     answer = -1
     dp = []
     
-    for cnt in range(1, 9): # 1개부터 8개까지 확인
+    for i in range(1, 9):
         all_case = set()
-        all_case.add(int(str(N) * cnt)) # 이어 붙여서 만드는 경우 넣기
+        all_case.add(int(str(N) * i))
         
-        for i in range(cnt - 1): # (1, n-1) 부터 (n-1, 1)까지 사칙연산
-            for op1 in dp[i]:
-                for op2 in dp[-i - 1]:
-                    all_case.add(op1 + op2)
-                    all_case.add(op1 * op2)
-                    all_case.add(op1 - op2)
-                    if op2 != 0:
-                        all_case.add(op1 / op2)
-                        
-        # 만든 집합에 number가 처음 나오는지 확인
+        for pair in range(0, i - 1):
+            for left in dp[pair]:
+                for right in dp[-1 - pair]:
+                    all_case.add(left + right)
+                    all_case.add(left - right)
+                    all_case.add(left * right)
+                    if right != 0:
+                        all_case.add(left / right)
         if number in all_case:
-            answer = cnt
+            answer = i
             break
-        dp.append(all_case)
-        
+        else:
+            dp.append(all_case)
+    
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(N, number):
+#     answer = -1
+#     dp = []
+    
+#     for cnt in range(1, 9): # 1개부터 8개까지 확인
+#         all_case = set()
+#         all_case.add(int(str(N) * cnt)) # 이어 붙여서 만드는 경우 넣기
+        
+#         for i in range(cnt - 1): # (1, n-1) 부터 (n-1, 1)까지 사칙연산
+#             for op1 in dp[i]:
+#                 for op2 in dp[-i - 1]:
+#                     all_case.add(op1 + op2)
+#                     all_case.add(op1 * op2)
+#                     all_case.add(op1 - op2)
+#                     if op2 != 0:
+#                         all_case.add(op1 / op2)
+                        
+#         # 만든 집합에 number가 처음 나오는지 확인
+#         if number in all_case:
+#             answer = cnt
+#             break
+#         dp.append(all_case)
+        
+#     return answer
