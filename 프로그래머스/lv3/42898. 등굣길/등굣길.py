00dec9daea20eb4c4c77fbd9f1a3de80bbd2eff4
leejiseop <lkj0245@naver.com>
2023-09-17 17:19:10 +0900

    [level 3] Title: 등굣길, Time: 2.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,13 @@
+def solution(m, n, puddles):
+    answer = 0
+    puddles = [[j, i] for [i, j] in puddles]
+    dp = [[0 for i in range(m + 1)] for j in range(n + 1)]
+    dp[1][1] = 1
+    
+    for x in range(1, n + 1):
+        for y in range(1, m + 1):
+            if [x, y] in puddles:
+                continue
+            dp[x][y] += dp[x-1][y] + dp[x][y-1]
+    
+    return dp[-1][-1] % 1000000007
