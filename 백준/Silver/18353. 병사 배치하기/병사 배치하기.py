4879e22a200075fe3a42b238d72257e97ca80445
leejiseop <lkj0245@naver.com>
2023-12-28 00:53:33 +0900

    [Silver II] Title: 병사 배치하기, Time: 696 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,17 +1,15 @@
-# 1:14
-
-INF = 987654321
+# 12:40
 
 n = int(input())
-soldiers = [0] + list(map(int, input().split()))
-dp = [0] * (n + 1)
+soldiers = list(map(int, input().split()))
+dp = [0] * n
+temp = 0
 
-# n2 안에
-
-for i in range(1, n + 1):
+for i in range(n):
   for j in range(i):
     if soldiers[i] < soldiers[j]:
-      dp[i] = max(dp[i], dp[j])
-  dp[i] += 1
+      temp = max(temp, dp[j])
+  dp[i] = temp + 1
+  temp = 0
 
 print(n - max(dp))
