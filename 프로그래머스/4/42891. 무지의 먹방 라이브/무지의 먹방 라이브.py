ef651954f00471f733386536380c64de45491d56
leejiseop <lkj0245@naver.com>
2024-01-27 02:06:13 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 184.70 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,26 +1,70 @@
-import heapq # 02:23
+# 2:00
+
+import heapq
 
 def solution(food_times, k):
     answer = -1
-    h = []
-    length = len(food_times)
-    prev = 0
+    remain_foods = len(food_times)
+    cycle = 0
+    hq = []
     
-    for i in range(length):
-        heapq.heappush(h, (food_times[i], i + 1)) # (남은 음식, 번호)
-    
-    while h:
-        now = h[0][0]
-        cycle = (now - prev) * length
-        if cycle <= k:
-            while h and h[0][0] == now:
-                heapq.heappop(h)
-                length -= 1
-            prev = now
-            k -= cycle
+    for i in range(remain_foods):
+        heapq.heappush(hq, [food_times[i], i + 1])
+        
+    while hq:
+        now = hq[0][0]
+        full = (now - cycle) * remain_foods
+        if full <= k:
+            k -= full
+            while hq and hq[0][0] == now:
+                heapq.heappop(hq)
+                remain_foods -= 1
+            cycle = now
         else:
-            h.sort(key = lambda x: x[1])
-            answer = h[k % length][1]
+            last = k % remain_foods
+            hq.sort(key = lambda x: x[1])
+            answer = hq[last][1]
             break
-    
+        
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import heapq
+
+# def solution(food_times, k):
+#     answer = -1
+#     h = []
+#     length = len(food_times)
+#     prev = 0
+    
+#     for i in range(length):
+#         heapq.heappush(h, (food_times[i], i + 1)) # (남은 음식, 번호)
+    
+#     while h:
+#         now = h[0][0]
+#         cycle = (now - prev) * length
+#         if cycle <= k:
+#             while h and h[0][0] == now:
+#                 heapq.heappop(h)
+#                 length -= 1
+#             prev = now
+#             k -= cycle
+#         else:
+#             h.sort(key = lambda x: x[1])
+#             answer = h[k % length][1]
+#             break
+    
+#     return answer
